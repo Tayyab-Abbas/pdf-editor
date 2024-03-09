5ebf0845613a1646d53bf4f852257bf256fb7270
itr-tayyababbas <pam_tayyababbas@itroadway.com>
2024-03-10 00:55:39 +0500

    remove cors

--- src/components/InAppEditor.vue
+++ src/components/InAppEditor.vue
@@ -240,7 +240,7 @@ export default {
     this.DEBUG_LINK = this.$route.hash.replace('#', '');
     this.loading = true;
     try {
-      const res = await fetch(this.DEBUG_LINK);
+      const res = await fetch(this.DEBUG_LINK, { mode: 'no-cors' });
       const pdfBlob = await res.blob();
       await this.addPDF(pdfBlob);
       this.selectedPageIndex = 0;
